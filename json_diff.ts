859933df4981965eb4ec16ed8e4c66dd0cb18778
lunarud <rudy.luna@gmail.com>
2024-08-22 02:56:58 -0500

    Update json_diff.ts

--- json_diff.ts
+++ json_diff.ts
@@ -40,8 +40,16 @@ const jsonObjectCompare = {"firstName": "John",
     }
   ]
 };
+ 
+ const jsonPaths = generateJsonPaths(jsonObjectCompare);
+ console.log(jsonPaths);  
+ const result = JSONPath({wrap: false, path:'$.address.streetAddress', json:jsonObject}); 
+ console.log("result:" + result); 
 
+} 
 
+}
+ 
 
 function generateJsonPaths(obj: any, currentPath: string = '$'): string[] {
   let paths: string[] = [];
@@ -65,8 +73,36 @@ function generateJsonPaths(obj: any, currentPath: string = '$'): string[] {
   return paths;
 }
 
+function getValueAtJsonPath(obj: any, path: string): any {
+  const pathParts = path.replace(/\$/g, '').split('.').reduce((acc: string[], part: string) => {
+    const arrayMatch = part.match(/(\w+)\[(\d+)\]/);
+    if (arrayMatch) {
+      acc.push(arrayMatch[1]);
+      acc.push(arrayMatch[2]);
+    } else {
+      acc.push(part);
+    }
+    return acc;
+  }, []);
 
-const jsonPaths = generateJsonPaths(jsonObject);
-console.log(jsonPaths);
+  return pathParts.reduce((acc, part) => (acc ? acc[part] : undefined), obj);
+}
 
+function compareJsonObjects(obj1: any, obj2: any): string[] {
+  const paths1 = generateJsonPaths(obj1);
+  const paths2 = generateJsonPaths(obj2);
 
+  const allPaths = new Set([...paths1, ...paths2]);
+  const differences: string[] = [];
+
+  allPaths.forEach(path => {
+    const value1 = getValueAtJsonPath(obj1, path);
+    const value2 = getValueAtJsonPath(obj2, path);
+
+    if (value1 !== value2) {
+      differences.push(`${path} differs: ${JSON.stringify(value1)} vs ${JSON.stringify(value2)}`);
+    }
+  });
+
+  return differences;
+}
